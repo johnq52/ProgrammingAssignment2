6354a0b8e89a0b3d1bc0b74028a65900bed76165
johnq52 <johnkat1@verizon.net>
2014-12-20 19:46:03 -0800

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions calculate the inverse of a square matrix.  
 
-## Write a short comment describing this function
+## Function is used to initialize square matrix.  Methods in function are used to calculate inverse of square matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
- m <- NULL
-        set <- function(y) {
+ m <- NULL      #reset to NULL when makeCacheMatrix is called
+        set <- function(y) {    #allows a new value to be substituted for prior object
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
-        list(set = set, get = get,
+        get <- function() x    # returns value of original matrix
+        setinverse <- function(solve) m <<- solve   #called by cacheSolve during the first function call
+		                                            #stores value using superassignment                  
+        getinverse <- function() m  #gets inverse when the inverse of the matrix has been calculated.
+        list(set = set, get = get,  #This is a list of internal functions to give calling function access
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function is used to calculate square matrix that was defined in the makeCacheMatrix function.  If the 
+## inverse was already calculated, function retrieves stored value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-	 m <- x$getinverse()
+	 m <- x$getinverse()   ##gets inverse of matrix if it has been calculated.
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
+        data <- x$get()    #gets original value of matrix.  The inverse has not yet been calculated.
+        m <- solve(data, ...)   #calculates inverse of a square matrix.
+        x$setinverse(m)         #calculated inverse of a square matrix
         m	
 }
